39b626940fa335505ac0f7e89c634b2af94e6946
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-07-09 03:16:24 -0500

    Failed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
 import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
 import {SharedModule} from "./shared/shared.module";
 import {TasksModule} from "./tasks/tasks.module";
 
@@ -18,7 +16,7 @@ import {TasksModule} from "./tasks/tasks.module";
   imports: [
     //CommonModule,
     BrowserModule,
-    FormsModule,
+    SharedModule,
     TasksModule
   ]
 })
